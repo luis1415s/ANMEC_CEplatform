4a8928af9cd819c85e5fcd847b1468d245cb4902
luis1415s <luiscarlos.vocacional5@gmail.com>
2020-01-12 19:50:14 -0600

    conexionDB

--- conex.php
+++ conex.php
@@ -1,5 +1,5 @@
 <?php
-	$conexion=mysqli_connect("localhost", "root", "", "anmeceval");
+	$conexion=mysqli_connect("localhost", "", "", "anmeceval");
 	if (mysqli_connect_errno()) {
 		print("Fallo la conexion");
 	}
